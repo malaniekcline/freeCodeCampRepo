8b92b5a452785576cfcb70b88bdd419553cb6000
Malanie Cline <160076381+malaniekcline@users.noreply.github.com>
2024-04-09 15:20:57 -0400

    Build a Periodic Table Database - Initial

---
+++ element.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+if [ -z "$1" ]; then
+  echo "Please provide an element as an argument."
+  exit 1
+fi
+
